c303ff81df887d58aee83346ac15a703da7122a2
mitchierichie <48261027+mitchierichie@users.noreply.github.com>
2020-07-10 09:17:44 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -106,11 +106,11 @@ fi
 initiate_lighsailScript() {  
   ZONE_ID=$(curl -X POST -H "Content-Type: application/json" -d "{\"domain\": \"${SITE_URL}\"}" https://nativerank.dev/cloudflareapi/zone_id)
   
-  printf -- "\n DEBUG: DEVSITE SLUG ${DEVSITE_SLUG} \n"
-  printf -- "\n DEBUG: SITEURL ${SITE_URL} \n"
-  printf -- "\n DEBUG: PUBLIC_IP ${PUBLIC_IP} \n"
+  #printf -- "\n DEBUG: DEVSITE SLUG ${DEVSITE_SLUG} \n"
+  #printf -- "\n DEBUG: SITEURL ${SITE_URL} \n"
+  #printf -- "\n DEBUG: PUBLIC_IP ${PUBLIC_IP} \n"
 
-  printf -- "\n Replace PUBLIC IP with production URL....... \n"
+  printf -- "\n Replace PUBLIC IP ${PUBLIC_IP} with production URL ${SITE_URL}....... \n"
   wp search-replace "${PUBLIC_IP}" "${SITE_URL}" --skip-plugins=w3-total-cache --all-tables --report-changed-only
   wp search-replace "nrdevsites.com" "nativerank.dev" --skip-plugins=w3-total-cache --all-tables --report-changed-only
   wp search-replace "www.nativerank.dev" "nativerank.dev" --skip-plugins=w3-total-cache --all-tables --report-changed-only
@@ -121,41 +121,42 @@ initiate_lighsailScript() {
   printf -- "\n Replacing devsite slug with production (unescaped) URL....... \n"
   wp search-replace "nativerank.dev/${DEVSITE_SLUG}" "${SITE_URL}" --skip-plugins=w3-total-cache --all-tables --report-changed-only
 
-  printf -- "\n Running the same replacements on Less and CSS....... \n"
+  printf -- "\n Running the same replacements on Less, CSS, Handlebars templates, and data.json....... \n"
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -name "*.less" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -name "*.less" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
+  ### .LESS ###
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -name "*.less" -exec sed -i "s/http:/https:/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -name "*.less" -exec sed -i "s/https:\/\/nativerank.dev/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -iname "*.less" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -iname "*.less" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -name "*.less" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -iname "*.less" -exec sed -i "s/http:/https:/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -iname "*.less" -exec sed -i "s/https:\/\/nativerank.dev/nativerank.dev/g" {} +
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -name "*.css" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -name "*.css" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/less/src/ -iname "*.less" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -name "*.css" -exec sed -i "s/http:/https:/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -name "*.css" -exec sed -i "s/https:\/\/nativerank.dev/nativerank.dev/g" {} +
+  ### .CSS ###
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -name "*.css" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -iname "*.css" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -iname "*.css" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
 
-  printf -- "\n Running the same replacements on data.json....... \n"
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -iname "*.css" -exec sed -i "s/http:/https:/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -iname "*.css" -exec sed -i "s/https:\/\/nativerank.dev/nativerank.dev/g" {} +
 
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/css/ -iname "*.css" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/ -name "data.json" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
+  ### DATA.JSON ###
 
-  printf -- "\n Running the same replacements for Handlebars templates....... \n"
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/ -iname "data.json" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}//g" {} +
 
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -name "*.hbs" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -name "*.hbs" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -name "*.hbs" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}/${SITE_URL}/g" {} +
-  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -name "*.hbs" -exec sed -i "s/http:\/\/${SITE_URL}/https:\/\/${SITE_URL}/g" {} +
+  ### .HBS ###
+
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -iname "*.hbs" -exec sed -i "s/nrdevsites.com/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -iname "*.hbs" -exec sed -i "s/www.nativerank.dev/nativerank.dev/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -iname "*.hbs" -exec sed -i "s/nativerank.dev\/${DEVSITE_SLUG}/${SITE_URL}/g" {} +
+  find /home/bitnami/apps/wordpress/htdocs/wp-content/themes/yootheme_child/templates/ -iname "*.hbs" -exec sed -i "s/http:\/\/${SITE_URL}/https:\/\/${SITE_URL}/g" {} +
 
   printf -- "\n Making it secure [http -> https]....... \n"
   
   wp search-replace "http://${SITE_URL}" "https://${SITE_URL}" --skip-plugins=w3-total-cache --all-tables --report-changed-only
-
-  printf -- "\n Setting site URL in WordPress....... \n"
 
   printf -- "\n Configuring WP Rocket plugin and setting WP_CACHE....... \n"
   wp redis disable
@@ -186,6 +187,8 @@ initiate_lighsailScript() {
 
   wp cache flush --skip-plugins=w3-total-cache
   wp plugin activate wp-rocket
+  
+  printf -- "\n Setting site URL in WordPress....... \n"
 
   wp option update siteurl "https://${SITE_URL}"
   wp option update home "https://${SITE_URL}"
